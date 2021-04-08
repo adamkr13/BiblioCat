ebd5005d99ab198780ea2a2a7efc16cbbfe93ec0
adamkr13 <adamreuter13@gmail.com>
2021-04-08 19:45:41 -0400

    Add project files.

---
+++ BiblioCat.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BiblioCat.WebMVC.Startup))]
+namespace BiblioCat.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
