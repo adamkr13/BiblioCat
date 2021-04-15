d0a2ceafe080195b03bd97eb9b2c3ee7bd947735
adamkr13 <adamreuter13@gmail.com>
2021-04-15 10:19:34 -0400

    complete authorbook controller

---
+++ BiblioCat.WebMVC/Controllers/TableJunctions/AuthorBookController.cs
@@ -0,0 +1,74 @@
+﻿using BiblioCat.Models.TableJunctions.AuthorBook;
+using BiblioCat.Services.TableJunctions;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiblioCat.WebMVC.Controllers.TableJunctions
+{
+    public class AuthorBookController : Controller
+    {
+        // GET: AuthorBook
+        public ActionResult Index()
+        {
+            var service = CreateAuthorBookService();
+            var model = service.GetAuthorBooks();
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AuthorBookCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateAuthorBookService();
+
+            if (service.CreateAuthorBook(model))
+            {
+                TempData["SaveResult"] = $"Author {model.AuthorId} was added to book {model.BookId}";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "The author could not be added to the book.");
+
+            return View(model);
+        }
+
+
+        public ActionResult Delete(int authorId, int bookId)
+        {
+            var service = CreateAuthorBookService();
+            var model = service.GetAuthorBookById(authorId, bookId);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int authorId, int bookId)
+        {
+            var service = CreateAuthorBookService();
+            service.DeleteAuthorBook(authorId, bookId);
+
+            return RedirectToAction("Index");
+        }
+
+        private AuthorBookService CreateAuthorBookService()
+        {
+            Guid userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new AuthorBookService(userId);
+            return service;
+        }
+    }
+}
