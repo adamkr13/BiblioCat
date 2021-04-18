83eae9995aa75a646ac71efefdb89ee0bc22b8fc
adamkr13 <adamreuter13@gmail.com>
2021-04-18 15:51:18 -0400

    add Convention views

--- BiblioCat.WebMVC/Controllers/ConventionController.cs
+++ BiblioCat.WebMVC/Controllers/ConventionController.cs
@@ -29,10 +29,92 @@ namespace BiblioCat.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(ConventionCreate model)
         {
+            if (!ModelState.IsValid) return View(model);
 
+            var service = CreateConventionService();
+
+            if (service.CreateConvention(model))
+            {
+                TempData["SaveResult"] = "The convention was created.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "The convention could not be created.");
+
+            return View(model);
         }
 
+        public ActionResult Details(int id)
+        {
+            var service = CreateConventionService();
+            var model = service.GetConventionById(id);
 
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateConventionService();
+            var detail = service.GetConventionById(id);
+
+            var model = new ConventionEdit
+            {
+                ConventionId = detail.ConventionId,
+                Name = detail.Name,
+                City = detail.City,
+                State = detail.State,
+                StartDate = detail.StartDate,
+                EndDate = detail.EndDate,
+                Hotel = detail.Hotel
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ConventionEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.ConventionId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateConventionService();
+
+            if (service.UpdateConvention(model))
+            {
+                TempData["SaveResult"] = "The convention was updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "The convention could not be updated.");
+
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var service = CreateConventionService();
+            var model = service.GetConventionById(id);
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateConventionService();
+            service.DeleteConvention(id);
+
+            TempData["SaveResult"] = "The convention was deleted.";
+
+            return RedirectToAction("Index");
+        }
 
 
 
